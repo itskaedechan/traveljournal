70d94f0e19ef5d3eaf393eedc92c3570ab093aea
Maria Anne San Andres <mariannetamsa@gmail.com>
2025-07-04 22:13:37 +0800

    Adding of meta tag and change of photo url for globe image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <img src="src/images/globe.png" alt="globe" className="mr-2" />
+      <img src="/photos/globe.png" alt="globe" className="mr-2" />
       <h1>my travel journal</h1>
     </header>
   );
