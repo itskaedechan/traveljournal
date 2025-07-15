0cd51ba76a2ca8abd9289962bd7d67ed31aacff3
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-07-16 05:47:06 +0800

    Adjusted scaling of page and images

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,16 +2,16 @@ import places from "../data/places.json";
 
 export default function Entry() {
   return (
-    <main className="grid grid-rows-3 justify-center max-w-prose mx-auto px-4 py-8">
+    <main className="grid grid-rows-3 justify-center max-w-screen-md mx-auto pt-8">
       {places.map((loc) => (
         <div key={loc.id} className="group min-h-[30px] overflow-auto">
           <img src={loc.img} alt={loc.place} className="place rounded-md" />
-          <div className="grid auto-rows-4 gap-0 ml-5 my-[2px] ">
+          <div className="grid auto-rows-4 gap-0 ml-8 my-[2px]">
             <div className="destination items-center">
               <img
                 src="photos/pin.png"
                 alt="marker"
-                className="max-w-[15px] max-h-[15px] mr-1"
+                className="max-w-[20px] max-h-[20px] mr-1"
               />
               <span className="font-100 tracking-[2px]">{loc.place}</span>
               <a
@@ -26,7 +26,6 @@ export default function Entry() {
             <h2 className="title">{loc.name}</h2>
             <p className="date">{loc.date}</p>
             <p className="text-justify">{loc.info}</p>
-            {/* <p>{loc.info}</p> max-w-md */}
           </div>
         </div>
       ))}
