06ec858fe53cc92647911567808214e45fc57e34
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-07-16 06:06:45 +0800

    Added padding bottom for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center">
+    <footer className="text-center pb-10">
       © 2025 San Andres development. All rights reserved. Layout inspired by
       Scrimba
     </footer>
